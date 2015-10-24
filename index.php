6ce811b2e9b99e85a60a032d77bf9843b627fbe7
Ubuntu <ubuntuserme@gmail.com>
2015-10-24 10:42:52 +0530

    initial files

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>vestibulum</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Vestibulum</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+         <!--  <a  class="btn btn-success">Register Now</a>
+          <span class="divider-vertical"></span> -->
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+            
+          </form>
+
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+
+   
+   <div id="myCarousel" class="carousel slide top-margin" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide img-responsive img-full" src="image/1.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Paramahansa Yogananda</h1>
+              <p>"Let my soul smile through my heart and my heart smile through my eyes, that I may scatter rich smiles in sad hearts"</p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide img-responsive img-full" src="image/2.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Leo Buscaglia</h1>
+              <p>"Too often we underestimate the power of a touch, a smile, a kind word, a listening ear, an honest compliment, or the smallest act of caring, all of which have the potential to turn a life around"</p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide img-responsive img-full" src="image/3.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Rashida Jones</h1>
+              <p>"Smiling is definitely one of the best beauty remedies. If you have a good sense of humor and a good approach to life, that's beautiful"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Viverra nullam!</h1>
+        <p>Dictumst a habitant vestibulum potenti commodo ac eros habitasse dictumst scelerisque at magnis adipiscing non semper adipiscing eleifend metus non orci ullamcorper a condimentum. Natoque rutrum a a tempor odio ad scelerisque netus aliquet curae id ultricies a eget. Condimentum a consectetur nec curabitur viverra nullam vel accumsan a condimentum iaculis felis ac a habitasse cum et imperdiet. Urna ridiculus quis.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="image/profile.png" class="img-responsive" />
+              <h4 class="text-align-center">scelerisque netus</h4>
+            </div>
+            <div class="col-md-4">
+              <img src="image/profile.png" class="img-responsive" />
+            <h4 class="text-align-center">consectetur netus</h4>
+            </div>
+            <div class="col-md-4">
+              <img src="image/profile.png" class="img-responsive" />
+            <h4 class="text-align-center">viverra netus</h4>
+            </div>
+          </div>
+          <div class="row">
+          <h3>Latest News</h3>
+          <div class="col-md-4">
+              <h4>Cras justo odio</h4>
+               <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam ras justo odio, dapibus ac facilisis in.eget quam ras justo odio, dapibus ac facilisis in.</p>
+              <a class="btn btn-default" href="#" role="button">View details »</a>
+            </div>
+            <div class="col-md-4">
+            <h4>dapibus ac</h4>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam ras justo odio, dapibus ac facilisis in. quam ras justo odio, dapibus ac facilisis in.</p>
+            <a class="btn btn-default" href="#" role="button">View details »</a>
+            </div>
+            <div class="col-md-4">
+             <h4>egestas eget</h4>
+             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam ras justo odio, dapibus ac Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam ras justo odio, dapibus ac facilisis in. facilisis in.</p>
+             <a class="btn btn-default" href="#" role="button">View details »</a>
+            </div>
+            
+          </div>
+       </div>
+        <div class="col-md-4">
+          <img src="image/p1.jpg" class="img-responsive">
+          <div class="inner-img">
+            <h2>Facilisis in</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          </div>
+         <!--  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
